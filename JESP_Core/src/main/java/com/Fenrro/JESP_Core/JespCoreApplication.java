40eaf4e7197b721a5afeb1d95b2ba2cbbca8ca91
Fenrro27 <kjba28sep@gmail.com>
2026-07-16 17:01:35 +0200

    Springboot

---
+++ JESP_Core/src/main/java/com/Fenrro/JESP_Core/JespCoreApplication.java
@@ -0,0 +1,15 @@
+package com.Fenrro.JESP_Core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class JespCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JespCoreApplication.class, args);
+	}
+
+}
